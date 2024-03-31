4076bb83bdb3f72bd592d577c68e370a2ea5d792
ajuhpark <ajuhpark@gmail.com>
2024-03-31 14:30:01 -0700

    Up to Merge html and webgl works

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -46666,7 +46666,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57674" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65368" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
